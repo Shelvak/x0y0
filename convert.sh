d39268db29211c73055fe50a2b9a6b58ad8e8380
Néstor Coppi <nestorcoppi@gmail.com>
2014-07-16 21:34:28 -0300

    Convert Script

---
+++ convert.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+SED_COMMAND='sed'
+if test "$(uname)" = "Darwin"; then
+  SED_COMMAND='gsed'
+fi
+
+
+read -p "Converting X0Y0 (mayus) in: " choice
+egrep -rl 'X0Y0' * | xargs $SED_COMMAND -i "s/X0Y0/$choice/g"
+
+read -p "Converting x0y0 (minus) in: " choice
+egrep -rl 'x0y0' * | xargs $SED_COMMAND -i "s/x0y0/$choice/g"
