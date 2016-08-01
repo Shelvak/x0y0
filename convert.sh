f993503ca94224baa402fdaeb86fb9fa13903d60
Néstor Coppi <nestorcoppi@gmail.com>
2016-08-01 19:30:31 -0300

    Generators

--- convert.sh
+++ convert.sh
@@ -11,3 +11,6 @@ egrep -rl 'X0Y0' * | xargs $SED_COMMAND -i "s/X0Y0/$choice/g"
 
 read -p "Converting x0y0 (minus) in: " choice
 egrep -rl 'x0y0' . | xargs $SED_COMMAND -i "s/x0y0/$choice/g"
+
+rm -Rf .git
+git init .
